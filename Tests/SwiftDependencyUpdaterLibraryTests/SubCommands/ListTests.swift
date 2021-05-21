1457f5efc9b2f016ea72081b1e22bb2a0c69fbf5
Steffen Kötte <steffen.koette@gmail.com>
2021-05-20 23:00:25 -0700

    Add invalid package test

--- Tests/SwiftDependencyUpdaterLibraryTests/SubCommands/ListTests.swift
+++ Tests/SwiftDependencyUpdaterLibraryTests/SubCommands/ListTests.swift
@@ -19,6 +19,18 @@ class ListTests: XCTestCase {
         XCTAssertEqual(result.output, "Could not get package data, swift package dump-package failed: error: root manifest not found")
     }
 
+    func testInvalidPackage() {
+        let folder = emptyFolderURL()
+        let packageSwift = temporaryFileURL(in: folder, name: "Package.swift")
+        createFile(at: packageSwift, content: TestUtils.emptyPackageSwiftFileContent)
+        let packageResolved = temporaryFileURL(in: folder, name: "Package.resolved")
+        createFile(at: packageResolved, content: TestUtils.emptyPackageResolvedFileContent)
+        let result = outputFromExecutionWith(arguments: ["list", folder.path])
+        XCTAssertEqual(result.exitCode, 1)
+        XCTAssertEqual(result.errorOutput, "")
+        XCTAssert(result.output.contains("Could not get package data, swift package dump-package failed"))
+    }
+
     func testNoDependencies() {
         let folder = createEmptySwiftPackage()
         let result = outputFromExecutionWith(arguments: ["list", folder.path])
