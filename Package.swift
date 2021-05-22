544971e85708408417f067586b60df9ea28d033f
Steffen Kötte <steffen.koette@gmail.com>
2021-05-22 10:44:40 -0700

    Bump swift-argument-parser to 0.4.3

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-argument-parser",
-            .upToNextMinor(from: "0.3.1")
+            .upToNextMinor(from: "0.4.3")
         ),
         .package(
             url: "https://github.com/Nef10/ShellOut.git",
