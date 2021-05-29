e170a4d5211912ab9a1d4333d002a94b2296c15b
Steffen Kötte <steffen.koette@gmail.com>
2021-05-29 11:48:14 -0700

    Bump version to 0.0.3

--- Sources/SwiftDependencyUpdaterLibrary/SwiftDependencyUpdater.swift
+++ Sources/SwiftDependencyUpdaterLibrary/SwiftDependencyUpdater.swift
@@ -5,7 +5,7 @@ public struct SwiftDependencyUpdater: ParsableCommand {
     public static var configuration = CommandConfiguration(
         commandName: "swift-dependency-updater",
         abstract: "A CLI tool to update Swift Pacakge Manager dependencies",
-        version: "0.0.2",
+        version: "0.0.3",
         subcommands: [ListCommand.self, UpdateCommand.self, GitHubCommand.self],
         defaultSubcommand: UpdateCommand.self
 
