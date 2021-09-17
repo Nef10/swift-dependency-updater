979bd00258354a01052958369830fc9e4bafa9cc
swift-dependency-updater <unknown>
2021-09-17 10:33:45 +0000

    Bump swift-argument-parser from 0.5.0 to 1.0.1

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-argument-parser",
-            .upToNextMinor(from: "0.5.0")
+            .upToNextMinor(from: "1.0.1")
         ),
         .package(
             url: "https://github.com/Nef10/ShellOut.git",
