6eac87d8c5ab1d56797110eccaa137ce57f6dbce
swift-dependency-updater <unknown>
2024-01-12 10:31:05 +0000

    Bump swift-argument-parser from 1.2.3 to 1.3.0

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-argument-parser",
-            .upToNextMinor(from: "1.2.0")
+            .upToNextMinor(from: "1.3.0")
         ),
         .package(
             url: "https://github.com/Nef10/ShellOut.git",
